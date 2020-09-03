e4db5760a25abdf9a83ab7298d79c3c440df0b59
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-09-03 21:50:49 +0800

    Remove debugging msg

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,8 +4,6 @@ import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-console.log(process.env);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
